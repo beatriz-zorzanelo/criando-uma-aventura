e2e451c39d0e49cb6b34ef069d5817b4b68102b8
beatriz-zorzanelo <beatriz.zorzanelo.camargo@escola.pr.gov.br>
2025-10-28 08:24:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const avanca = document.querySelectorALL('.btn-proximo');
 
-avanca.forEach(button =>){
-  button.addEventListener('click', function() {
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
      const atual = document.querySelector('.ativo');
-    }
-}
+     const proximoPasso ='passo-'+ this.getATTRIbut('data-proxima');
+  
+       atual.classList.remove(ativo);
+        document.getE1ementById(proximoPasso). classList.add('ativo') ;
+       constructor(parameters) 
+      
+    })
+  })
+
+  
+
+
